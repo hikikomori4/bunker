803a710cebde47005cc72b0d6234767018c7498d
Max <regforblank@gmail.com>
2021-04-26 19:55:24 +0300

    floor moving

--- bunker.py
+++ bunker.py
@@ -7,31 +7,31 @@ floor1 = {
         {
             'name': 'Комната 1',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room02', 's': 'room04', 'w': 'noway'}
+            'goto': {'n': 'noway', 'e': 'room02', 's': 'room04', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room02':
         {
             'name': 'Комната 2',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room03', 's': 'room05', 'w': 'room01'}
+            'goto': {'n': 'noway', 'e': 'room03', 's': 'room05', 'w': 'room01', 'u': 'noway', 'd': 'noway'}
         },
     'room03':
         {
             'name': 'Комната 3',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'noway', 's': 'room06', 'w': 'room02'}
+            'goto': {'n': 'noway', 'e': 'noway', 's': 'room06', 'w': 'room02', 'u': 'noway', 'd': 'noway'}
         },
    'room04':
         {
             'name': 'Комната 4',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room01', 'e': 'room05', 's': 'room07', 'w': 'noway'}
+            'goto': {'n': 'room01', 'e': 'room05', 's': 'room07', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room05':
         {
             'name': 'Комната 5',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room02', 'e': 'room06', 's': 'room08', 'w': 'room04'}
+            'goto': {'n': 'room02', 'e': 'room06', 's': 'room08', 'w': 'room04', 'u': 'noway', 'd': 'noway'}
         },
     'room06':
         {
@@ -43,19 +43,19 @@ floor1 = {
         {
             'name': 'Комната 7',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room04', 'e': 'room08', 's': 'noway', 'w': 'noway'}
+            'goto': {'n': 'room04', 'e': 'room08', 's': 'noway', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room08':
         {
             'name': 'Комната 8',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room05', 'e': 'room09', 's': 'noway', 'w': 'room07'}
+            'goto': {'n': 'room05', 'e': 'room09', 's': 'noway', 'w': 'room07', 'u': 'noway', 'd': 'noway'}
         },
     'room09':
         {
             'name': 'Комната 9',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room06', 'e': 'noway', 's': 'noway', 'w': 'room08'}
+            'goto': {'n': 'room06', 'e': 'noway', 's': 'noway', 'w': 'room08', 'u': 'noway', 'd': 'noway'}
         }
 }
 
@@ -65,31 +65,31 @@ floor2 = {
         {
             'name': 'Комната 10',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room11', 's': 'room13', 'w': 'noway'}
+            'goto': {'n': 'noway', 'e': 'room11', 's': 'room13', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room11':
         {
             'name': 'Комната 11',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room12', 's': 'room14', 'w': 'room10'}
+            'goto': {'n': 'noway', 'e': 'room12', 's': 'room14', 'w': 'room10', 'u': 'noway', 'd': 'noway'}
         },
     'room12':
         {
             'name': 'Комната 12',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'noway', 's': 'room15', 'w': 'room11'}
+            'goto': {'n': 'noway', 'e': 'noway', 's': 'room15', 'w': 'room11', 'u': 'noway', 'd': 'noway'}
         },
    'room13':
         {
             'name': 'Комната 13',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room10', 'e': 'room14', 's': 'room16', 'w': 'noway'}
+            'goto': {'n': 'room10', 'e': 'room14', 's': 'room16', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room14':
         {
             'name': 'Комната 14',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room11', 'e': 'room15', 's': 'room17', 'w': 'room13'}
+            'goto': {'n': 'room11', 'e': 'room15', 's': 'room17', 'w': 'room13', 'u': 'noway', 'd': 'noway'}
         },
     'room15':
         {
@@ -101,19 +101,19 @@ floor2 = {
         {
             'name': 'Комната 16',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room13', 'e': 'room17', 's': 'noway', 'w': 'noway'}
+            'goto': {'n': 'room13', 'e': 'room17', 's': 'noway', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room17':
         {
             'name': 'Комната 17',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room14', 'e': 'room18', 's': 'noway', 'w': 'room16'}
+            'goto': {'n': 'room14', 'e': 'room18', 's': 'noway', 'w': 'room16', 'u': 'noway', 'd': 'noway'}
         },
     'room18':
         {
             'name': 'Комната 18',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room15', 'e': 'noway', 's': 'noway', 'w': 'room17'}
+            'goto': {'n': 'room15', 'e': 'noway', 's': 'noway', 'w': 'room17', 'u': 'noway', 'd': 'noway'}
         }
 }
 
@@ -122,31 +122,31 @@ floor3 = {
         {
             'name': 'Комната 19',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room20', 's': 'room22', 'w': 'noway'}
+            'goto': {'n': 'noway', 'e': 'room20', 's': 'room22', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room20':
         {
             'name': 'Комната 20',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'room21', 's': 'room23', 'w': 'room19'}
+            'goto': {'n': 'noway', 'e': 'room21', 's': 'room23', 'w': 'room19', 'u': 'noway', 'd': 'noway'}
         },
     'room21':
         {
             'name': 'Комната 21',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'noway', 'e': 'noway', 's': 'room24', 'w': 'room20'}
+            'goto': {'n': 'noway', 'e': 'noway', 's': 'room24', 'w': 'room20', 'u': 'noway', 'd': 'noway'}
         },
    'room22':
         {
             'name': 'Комната 22',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room19', 'e': 'room23', 's': 'room25', 'w': 'noway'}
+            'goto': {'n': 'room19', 'e': 'room23', 's': 'room25', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room23':
         {
             'name': 'Комната 23',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room20', 'e': 'room24', 's': 'room26', 'w': 'room22'}
+            'goto': {'n': 'room20', 'e': 'room24', 's': 'room26', 'w': 'room22', 'u': 'noway', 'd': 'noway'}
         },
     'room24':
         {
@@ -158,19 +158,19 @@ floor3 = {
         {
             'name': 'Комната 25',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room22', 'e': 'room26', 's': 'noway', 'w': 'noway'}
+            'goto': {'n': 'room22', 'e': 'room26', 's': 'noway', 'w': 'noway', 'u': 'noway', 'd': 'noway'}
         },
     'room26':
         {
             'name': 'Комната 26',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room23', 'e': 'room27', 's': 'noway', 'w': 'room25'}
+            'goto': {'n': 'room23', 'e': 'room27', 's': 'noway', 'w': 'room25', 'u': 'noway', 'd': 'noway'}
         },
     'room27':
         {
             'name': 'Комната 27',
             'desc': 'Пустое помещение с серыми стенами.',
-            'goto': {'n': 'room24', 'e': 'noway', 's': 'noway', 'w': 'room26'}
+            'goto': {'n': 'room24', 'e': 'noway', 's': 'noway', 'w': 'room26', 'u': 'noway', 'd': 'noway'}
         }
 }
 
@@ -203,15 +203,25 @@ you_are_here = current_floor[current_room]
 
 def gogogo():
     global you_are_here
-
-    print(you_are_here['goto'][cmd])
+    global current_room
+    global current_floor
 
     if you_are_here['goto'][cmd] != 'noway':
         print("Вы перешли в " + you_are_here['goto'][cmd] + "...")
+
+        # Хотелось бы избаавиться от этого громоздкого фрагмента с проверкой current_floor.
+        if you_are_here == 'room06':
+            current_floor = floor1
+            print('Вы переместились на лифте на этаж 1')
+        if you_are_here == 'room15':
+            current_floor = floor2
+            print('Вы переместились на лифте на этаж 2')
+        if you_are_here == 'room24':
+            current_floor = floor3
+            print('Вы переместились на лифте на этаж 3')
+
+        # print(current_floor)
         you_are_here = current_floor[you_are_here['goto'][cmd]]
-
-
-
 
     else:
         print( str(you_are_here['goto'][cmd]) + '. Сюда хода нет!')
@@ -225,22 +235,9 @@ def go_up():
     if you_are_here['goto'][cmd] != 'noway':
         you_are_here = current_floor[you_are_here['goto'][cmd]]
 
-        if you_are_here == 'room06':
-            current_floor = floor1
-            print('Вы переместились на лифте на этаж 1')
-        if you_are_here == 'room15':
-            current_floor = floor2
-            print('Вы переместились на лифте на этаж 2')
-        if you_are_here == 'room24':
-            current_floor = floor3
-            print('Вы переместились на лифте на этаж 3')
 
-        else:
-            print('В этой комнате нет хода на другой этаж!')
-
-
-
-
+        # else:
+        #     print('В этой комнате нет хода на другой этаж!')
 
 
     # print(you_are_here['goto'][cmd])
@@ -312,8 +309,8 @@ actions = {
     's': gogogo,
     'w': gogogo,
 
-    'u': go_up,
-    'd': go_down,
+    'u': gogogo,
+    'd': gogogo,
     'l': look,
     't': take,
     'h': hlp,
